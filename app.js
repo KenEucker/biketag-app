4c236c9dee19f62aa043b17d759a1d2f62e1c842
Ken Eucker <keneucker@gmail.com>
2020-11-27 21:45:58 -0800

    Added the barebones starter app, using sexpress.

---
+++ app.js
@@ -0,0 +1,46 @@
+const merge = require('deepmerge')
+const sexpress = require('sexpress')
+
+/// BikeTag App specific configuration filters
+// const publicConfigFilter = (publicConfig, appConfig, subdomain) => {
+//     const subdomains = Object.keys(appConfig.subdomains)
+
+//     publicConfig.subdomains = Object.values(appConfig.subdomains).reduce(
+//         (out, subdomainInformation, index) => {
+//             const subdomainName = subdomains[index]
+//             const subdomainConfig = publicConfig.subdomains[subdomainName]
+//             publicConfig.supportedRegions = appConfig.supportedRegions
+
+//             // console.log({subdomainConfig})
+//             const pageData = merge(subdomainConfig, {
+//                 location: subdomainInformation.location,
+//                 easter: subdomainInformation.easter,
+//                 tagline: subdomainInformation.tagline,
+//                 jingle: subdomainInformation.jingle,
+//                 region: subdomainInformation.region,
+//                 readonly: subdomainInformation.readonly,
+//                 newGameImage: subdomainInformation.newGameImage,
+//                 reddit: {
+//                     subreddit: subdomainInformation.reddit.redditSubreddit,
+//                 },
+//             })
+
+//             out[subdomainName] = pageData
+
+//             if (subdomain === subdomainName) {
+//                 publicConfig.page = pageData
+//             }
+
+//             return out
+//         },
+//         {},
+//     )
+
+//     return publicConfig
+// }
+
+const app = sexpress({
+    // publicConfigFilter,
+})
+
+app.run()
